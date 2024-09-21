0f50b900bc9695417f95ae1d7acf3f85321337e0
Joseph Delgado <delgadojoseph271@gmail.com>
2024-09-21 17:03:22 -0500

    anaway

--- Juego/src/utils/desactivarCartas.py
+++
@@ -1,5 +0,0 @@
-def desactivarCartas(matriz, valor_pares):
-    for fila in matriz:
-        for carta in fila:
-            if carta.valor != valor_pares:
-                carta.estado = False  # Desactivar cartas que no coinciden
--- Juego/src/utils/marcarCartasPares.py
+++
@@ -1,5 +0,0 @@
-def marcarCartasPares(matriz, valor_pares):
-    for fila in matriz:
-        for carta in fila:
-            if carta.valor == valor_pares:
-                carta.no_cambiar_estado = True  # Marcar como parte de un par
